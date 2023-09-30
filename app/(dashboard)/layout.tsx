c587c11b94753543d0236a67302b95f185ac3b81
omznc <contact@omarzunic.com>
2023-09-30 19:03:46 +0200

    misc: sticky navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -23,48 +23,51 @@ export default async function Layout({ children }: { children: ReactNode }) {
 
 	return (
 		<AuthProvider session={session}>
-			<div className='flex flex-col items-center h-[100dvh] w-full'>
-				{/* beta bar */}
-				{env.IS_BETA && (
-					<div className='flex w-full justify-center bg-primary text-white'>
-						<p className='p-2 text-center'>
-							<strong>LiveLaunch Dashboard is in beta.</strong> If
-							you find any bugs, report them{' '}
-							<Link
-								href={'https://discord.gg/nztN2FXe7A'}
-								className='font-bold'
-								target={'_blank'}
-							>
-								here
-							</Link>
-							.
-						</p>
-					</div>
-				)}
-				<div className='hidden flex-row w-full justify-between border-b md:flex'>
-					<div className='flex h-16 items-center px-4'>
-						<GuildSwitcher guilds={guilds} />
-						<Nav className='mx-6' />
-					</div>
-					<div className='flex h-16 items-center gap-4 px-4'>
-						<ThemeToggle />
-						<User />
-					</div>
-				</div>
-				<div className='flex w-full flex-col md:hidden gap-4'>
-					<div className='flex h-16 items-center gap-2 justify-between px-4'>
-						<div className='flex items-center'>
-							<GuildSwitcher guilds={guilds} />
+			<div className='flex flex-col items-center min-h-screen h-full max-h-screen w-full'>
+				<div className='sticky top-0 w-full bg-background z-50'>
+					{env.IS_BETA && (
+						<div className='flex w-full justify-center bg-primary text-white'>
+							<p className='p-2 text-center'>
+								<strong>
+									LiveLaunch Dashboard is in beta.
+								</strong>{' '}
+								If you find any bugs, report them{' '}
+								<Link
+									href={'https://discord.gg/nztN2FXe7A'}
+									className='font-bold'
+									target={'_blank'}
+								>
+									here
+								</Link>
+								.
+							</p>
 						</div>
-						<div className='flex items-center gap-2'>
+					)}
+					<div className='hidden flex-row w-full justify-between border-b md:flex'>
+						<div className='flex h-16 items-center px-4'>
+							<GuildSwitcher guilds={guilds} />
+							<Nav className='mx-6' />
+						</div>
+						<div className='flex h-16 items-center gap-4 px-4'>
 							<ThemeToggle />
 							<User />
 						</div>
 					</div>
-					<Nav className='mx-6' />
+					<div className='flex w-full flex-col md:hidden gap-4'>
+						<div className='flex h-16 items-center gap-2 justify-between px-4'>
+							<div className='flex items-center'>
+								<GuildSwitcher guilds={guilds} />
+							</div>
+							<div className='flex items-center gap-2'>
+								<ThemeToggle />
+								<User />
+							</div>
+						</div>
+						<Nav className='mx-6' />
+					</div>
 				</div>
-				<div className='w-full h-full overflow-auto flex p-8 mb-8 justify-center'>
-					<div className='flex flex-col pb-2 w-full max-w-[1000px] transition-all'>
+				<div className='w-full flex p-8 justify-center'>
+					<div className='w-full max-w-[1200px] flex flex-col gap-8'>
 						{children}
 					</div>
 				</div>
