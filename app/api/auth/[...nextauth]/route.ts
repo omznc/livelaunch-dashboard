b5f0621bec897d14897581f6e56fcc9d3240e721
omznc <contact@omarzunic.com>
2023-10-13 16:41:37 +0200

    fix: refresh tokens

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,7 @@ import env from '@env';
 import prisma from '@lib/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { AxiomRequest, withAxiom } from 'next-axiom';
+import { Account } from '@prisma/client';
 
 export const authOptions: AuthOptions = {
 	// @ts-ignore
@@ -29,11 +30,13 @@ export const authOptions: AuthOptions = {
 						userId: session.user.id,
 					},
 				});
-				if (account) {
+				if (account && account.expires_at < Date.now()) {
+					session.account = await refreshDiscordToken(account);
+				} else {
 					session.account = account;
 				}
 			}
-
+			if (!session.account || !session.user) return null;
 			return session;
 		},
 	},
@@ -44,8 +47,6 @@ export const authOptions: AuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST };
-
 export const GET = withAxiom(
 	async (req: AxiomRequest, res) => await handler(req, res)
 );
@@ -53,3 +54,37 @@ export const GET = withAxiom(
 export const POST = withAxiom(
 	async (req: AxiomRequest, res) => await handler(req, res)
 );
+
+const refreshDiscordToken = async (account: Account) => {
+	const response = await fetch(`https://discord.com/api/oauth2/token`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: new URLSearchParams({
+			client_id: env.NEXT_PUBLIC_DISCORD_CLIENT_ID,
+			client_secret: env.DISCORD_CLIENT_SECRET,
+			grant_type: 'refresh_token',
+			refresh_token: account.refresh_token,
+		}),
+	});
+	const data = await response.json();
+	if (data.error) {
+		await prisma.account.delete({
+			where: {
+				id: account.id,
+			},
+		});
+		return null;
+	}
+	return await prisma.account.update({
+		where: {
+			id: account.id,
+		},
+		data: {
+			access_token: data.access_token,
+			refresh_token: data.refresh_token,
+			expires_at: Date.now() / 1000 + data.expires_in,
+		},
+	});
+};
