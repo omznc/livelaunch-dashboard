3ec01dbf1076bfece4da067b3913ae97af0d4612
Omar Zunic <contact@omarzunic.com>
2025-09-13 00:26:45 +0200

    fix: reuse prisma client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { PrismaClient } from '@prisma/client';
 import env from '@env';
-
-const prisma = new PrismaClient();
+import prisma from './prisma';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
