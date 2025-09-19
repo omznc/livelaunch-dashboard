a355557f75c97bda66207eff3f274bcd9d2a690a
Omar Zunic <contact@omarzunic.com>
2025-09-19 19:43:15 +0200

    fix: session handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,23 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import { env } from 'process';
 import { headers } from 'next/headers';
+import type { Session } from 'better-auth';
+import env from '@env';
 
 export async function middleware(request: NextRequest) {
   const resp = await fetch(`${env.PUBLIC_URL}/api/auth/get-session`, {
     headers: await headers(),
   });
 
-  const session = await resp.json();
+  let session: Session | null = null;
+
+  if (resp.ok) {
+    try {
+      session = await resp.json();
+    } catch {
+      console.error('Could not parse session');
+    }
+  }
 
   if (!session) {
     return NextResponse.redirect(new URL(`/login`, request.url));
